95c842337a0342124ad4b7bcd5225af80c6fd573
Diego Macias <djoromac@hotmail.com>
2022-12-23 15:46:54 -0500

    Clase de TC39

--- TC39.js
+++ TC39.js
@@ -2,6 +2,13 @@
 
 Es un grupo de hackers, académicos, personas afines al lenguaje de prgoramación que se encarga de que el lenguaje tenga actualizaciones en cada momento
 ESNEXT es la forma de proponer nuevas reglas y formas de trabajar el lenguaje.
+Tiene 5 stages el implementar una idea al estandar:
+Stage 0 : StrawPerson, idea
+Stage 1 : Proposal, proponer la idea
+Stage 2 : Draft, se crea un borrador para ver cómo implementarlo, qué impacto tendrá
+Stage 3 : Candidate, se elige un candidato para ver si funciona dicha función
+Stage 4 : Se implementa la idea al nuevo estandarte de ECMAScript
+Puede salir en Junio, el mes elegido para publicar las versiones nuevas.
 
 
 */
