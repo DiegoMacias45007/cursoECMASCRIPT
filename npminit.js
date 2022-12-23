faeae0fe204039cd729d2951dcab4d38a9be2025
Diego Macias <djoromac@hotmail.com>
2022-12-23 16:05:21 -0500

    Tips para iniciar un node package manager

---
+++ npminit.js
@@ -0,0 +1,13 @@
+/*
+        Tips para iniciar un NPM:
+
+        package name: el nombre de tu carpeta
+        Version: Idealmente 1.0.0
+        Description: descripcion breve de tu proyecto
+        entry point: default
+        test command: vacio
+        git repository: tu repositorio de git
+        keywords: de 3 a 5 palabras clave de tu proyecto
+        author: Diego Macias <djoromac@hotmail.com>
+        license: MIT
+*/
