f5c60ca8c758751cf4da2be8791948b2248df880
Diego Macias <djoromac@hotmail.com>
2022-12-23 15:41:35 -0500

    Primeras dos clases

---
+++ TC39.js
@@ -0,0 +1,7 @@
+/*¿Qué es el TC39?
+
+Es un grupo de hackers, académicos, personas afines al lenguaje de prgoramación que se encarga de que el lenguaje tenga actualizaciones en cada momento
+ESNEXT es la forma de proponer nuevas reglas y formas de trabajar el lenguaje.
+
+
+*/
---
+++ historia.js
@@ -0,0 +1,34 @@
+/*
+En 1950 fue el inicio de las computadoras, las cuales surgieron para las guerras
+Sin embargo, hasta 1969 se creó la red ARPANET donde se comunicaron por primera vez dos computadoras, de igual manera para aspectos de guerra
+De 1970 hasta 1990 se exploró el cómo funcionaba ARPANET y hasta 1990 dio la pauta a que existiera el internet con el término mundialmente conocido como WORLD WIDE WEB
+
+En 1993 nace el primer Navegador, Universitarios pioneros escribieron un software siemple que inició una revolución en la tecnología.
+Marc Andreessen le dió la vida a una gran empresa: NETSCAPE
+
+Netscape tenía un navegador que podía acceder al internet con un HTML simple HTML
+A partir de que Microsoft quería entrar al mundo del internet, creó Internet Explorer, provocando las Browser Wars.
+Buscaban tratar de mejorar las funciones y jugaron bromas entre ellos.
+
+En dichas guerras surgieron cosas muy buenas, como CSS conceptos para la empreza Mozilla y JS
+Dentro de netscape existió JAVASCRIPT en 1995, llamado Mocha, luego LiveScript, y finalmente JavaScript
+Microsoft no se quedó atrás, lanzando JScript.
+
+De ahí surgió ECMA (European Computer Manufacturer Association), un comité que se encargaba de estipular las normas
+ECMASCRIPT es la base de reglas estandar que deben seguir los lenguajes de programación.
+En 1997 surgió ECMA 262.
+
+El lenguaje de programación ha evolucionado gracias a ello: 
+1997 ES1
+1998 ES2
+1999 ES3
+2000 ES4 (Abandonado)
+2005 ES5
+2015 ES6
+2016 ES7
+2017 ES8
+2018 ES9
+2019 ES10
+
+
+*/
